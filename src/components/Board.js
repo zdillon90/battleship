270fe0a02e22f59fe68f71f973815596445e6683
Zach Dillon <zdillon90@gmail.com>
2019-07-23 21:11:11 -0400

    Update Board.js
    Starting to loop through the elements that make up the table. Will need to make the block into a seperate component later on.

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,32 +3,47 @@ import styled from 'styled-components';
 
 function Board() {
   const Container = styled.div`
-    display: grid;
-    grid-template-columns: 200px 200px 200px;
+    display: table;
     background-color: #2196f3;
     padding: 10px;
   `;
 
+  const Row = styled.div`
+    display: table-row;
+  `;
+
   const Block = styled.div`
+    display: table-cell;
     background-color: rgba(255, 255, 255, 0.8);
     border: 1px solid rgba(0, 0, 0, 0.8);
-    padding: 20px;
-    font-size: 30px;
+    font-size: 40px;
     text-align: center;
+    border-width: thin;
+    padding-left: 10px;
+    padding-right: 10px;
   `;
 
   return (
     <div>
       <Container>
-        <Block>1</Block>
-        <Block>2</Block>
-        <Block>3</Block>
-        <Block>4</Block>
-        <Block>5</Block>
-        <Block>6</Block>
-        <Block>7</Block>
-        <Block>8</Block>
-        <Block>9</Block>
+        <Row>
+          {(function() {
+            const rows = [];
+            for (let i = 0; i < 10; i += 1) {
+              rows.push(<Block key={i}>&nbsp;&nbsp;</Block>);
+            }
+            return rows;
+          })()}
+        </Row>
+        <Row>
+          {(function() {
+            const rows = [];
+            for (let i = 0; i < 10; i += 1) {
+              rows.push(<Block key={i}>&nbsp;&nbsp;</Block>);
+            }
+            return rows;
+          })()}
+        </Row>
       </Container>
     </div>
   );
