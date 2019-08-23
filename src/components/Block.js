4cfdf104cfe8ba475915ce0a99f391afcaa812ec
Zach Dillon <zdillon90@gmail.com>
2019-08-23 18:27:48 -0400

    Made Ships
    Created the ship options. Working on getting the styling started for components.

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
+import { StyledBlock } from './styles/StyledBlock';
+import { SHIPS } from '../ships';
 
 const Block = ({ type }) => (
-  // const coloredBlock = styled.div`
-  //   display: table-cell;
-  //   background-color: rgba(255, 255, 255, 0.8);
-  //   border: 1px solid rgba(0, 0, 0, 0.8);
-  //   font-size: 40px;
-  //   text-align: center;
-  //   border-width: thin;
-  //   padding-left: 10px;
-  //   padding-right: 10px;
-  // `;
-
-  <div>Block</div>
+  <StyledBlock type={type} color={SHIPS[type].color}>
+    Block
+  </StyledBlock>
 );
 
 export default Block;
+
+// const coloredBlock = styled.div`
+//   display: table-cell;
+//   background-color: rgba(255, 255, 255, 0.8);
+//   border: 1px solid rgba(0, 0, 0, 0.8);
+//   font-size: 40px;
+//   text-align: center;
+//   border-width: thin;
+//   padding-left: 10px;
+//   padding-right: 10px;
+// `;
---
+++ src/components/styles/StyledBlock.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StyledBlock = styled.div`
+  width: auto;
+  background: rgba(${props => props.color}, 0.8);
+`;
