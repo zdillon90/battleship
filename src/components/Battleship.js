aad45d09cff87607a7dbea562985d01f1ce53827
Zach Dillon <zdillon90@gmail.com>
2019-09-19 22:11:40 -0400

    Working on placing rest of blocks for ship
    The ship has a star block, just need to iterate on that block and add blocks either blow or to the right of the ship to complete the ship.

--- src/components/Battleship.js
+++ src/components/Battleship.js
@@ -35,6 +35,10 @@ function Battleship() {
     return Math.floor(Math.random() * (newMax - newMin + 1)) + newMin;
   };
 
+  const checkShipPalcement = () => {
+    // Check to see if block is filled first
+  };
+
   const makeShip = name => {
     const shipDirection = Math.random() < 0.5 ? 'vertical' : 'horizontal';
     let blockNumber = null;
@@ -47,31 +51,42 @@ function Battleship() {
     } else if (name === 'Patrol Boat') {
       blockNumber = 2;
     }
-    console.log(blockNumber);
-    const blocks = [];
-    for (let i = 0; i < blockNumber; i += 1) {
-      blocks.push(name.substring(0, 1));
-    }
-    console.log(blocks);
     // For them not to overlap I haft to mark the blocks with the
     // current ship and do a check, if it hits a block on creation
     // it starts over on the entire placement.
-    let positionRow = null;
-    let positionColum = null;
+    let startPositionRow = null;
+    let startPositionColum = null;
     if (shipDirection === 'vertical') {
-      positionRow = getRandomInt(0, 10 - blockNumber);
-      positionColum = getRandomInt(0, 10);
+      startPositionRow = getRandomInt(0, 10 - blockNumber);
+      startPositionColum = getRandomInt(0, 10);
     } else if (shipDirection === 'horizontal') {
-      positionRow = getRandomInt(0, 10);
-      positionColum = getRandomInt(0, 10 - blockNumber);
+      startPositionRow = getRandomInt(0, 10);
+      startPositionColum = getRandomInt(0, 10 - blockNumber);
+    }
+    const blocks = [];
+    for (let i = 0; i < blockNumber; i += 1) {
+      // If the block is the first then use the start position
+      const block = {};
+      block.letter = name.substring(0, 1);
+      if (i === 0) {
+        block.row = startPositionRow;
+        block.colum = startPositionColum;
+      } else if (i !== 0 && shipDirection === 'vertical') {
+        // vertical, block has to be one lower than the last
+      } else if (i !== 0 && shipDirection === 'horizontal') {
+        // horizontal, block has to be one right right than the last
+      }
+      block.row = null;
+      block.colum = null;
+      blocks.push(block);
     }
     const ship = {
       name,
       blocks,
       color: '128, 128, 128',
       shipDirection,
-      positionRow,
-      positionColum,
+      startPositionRow,
+      startPositionColum,
     };
     return ship;
   };
@@ -90,6 +105,7 @@ function Battleship() {
     });
     shipNames.forEach(shipName => {
       const ship = makeShip(shipName);
+      // check ship here before adding it
       addShip(ship);
     });
   };
