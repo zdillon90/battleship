ae0ef03e36cd25ebc6b9430589c4b4c600ec8678
Zach Dillon <zdillon90@gmail.com>
2019-10-07 21:02:03 -0400

    Added Firebase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { StoreProvider } from 'easy-peasy';
+import * as firebase from 'firebase';
 import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBAXSrRjyflBJQiECJ8J-ZMaKKGfV2H8FI',
+  authDomain: 'battleship-d697e.firebaseapp.com',
+  databaseURL: 'https://battleship-d697e.firebaseio.com',
+  projectId: 'battleship-d697e',
+  storageBucket: '',
+  messagingSenderId: '623731397978',
+  appId: '1:623731397978:web:d85fb6a20393432ff28f63',
+  measurementId: 'G-4PNKHXTM92',
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <StoreProvider store={store}>
