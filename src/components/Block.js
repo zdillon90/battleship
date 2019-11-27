99cb93abf7ffee29d39483795c377739b44ec467
Zach Dillon <zdillon90@gmail.com>
2019-11-26 21:45:48 -0500

    The hit block works

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,30 +1,38 @@
 import React, { useState } from 'react';
 import { StyledBlock } from './styles/StyledBlock';
+import { StyledHitBlock } from './styles/StyledHitBlock';
 import { SHIPS } from '../ships';
 
 function Block({ type, clickable, ship }) {
   const [select, setSelect] = useState(null);
-  const blockPressed = id => {
-    console.log(id);
-    if (id === '1') {
-      // console.log('Red');
-      return 'Red';
+  const blockPressed = () => {
+    console.log(type);
+    if (type === 1) {
+      console.log('hit');
+      setSelect('hit');
+    } else {
+      setSelect('miss');
     }
-    // console.log('White');
-    return 'White';
   };
-  return (
-    <StyledBlock
-      clickable={clickable}
-      onClick={e => blockPressed(e.target.attributes.type.value)}
-      type={type}
-      color={ship === 'clear' ? SHIPS[type].color : '128, 128, 128'}
-      // TODO Remove this switch
-      select={select}
-    />
-    // <StyledHitBlock />
-    // <StyledMissBlock />
-  );
+
+  switch (select) {
+    case 'hit':
+      return <StyledHitBlock />;
+    default:
+      return (
+        <StyledBlock
+          clickable={clickable}
+          onClick={e => blockPressed(e.target.attributes.type.value)}
+          type={type}
+          color={ship === 'clear' ? SHIPS[type].color : '128, 128, 128'}
+          // TODO Remove this switch
+          select={select}
+        />
+      );
+  }
+  // <StyledHitBlock />
+  // <StyledMissBlock />
+  // Switch which block style is shown
 }
 
 export default Block;
--- src/components/styles/StyledHitBlock.js
+++ src/components/styles/StyledHitBlock.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledHitBlock = styled.button`
   width: auto;
-  background-color: #bbb;
+  background-color: #ff0000;
   border-radius: 50%;
   display: inline-block;
 `;
