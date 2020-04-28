9c1a1ec657523d362c24604a329072f92b29e10c
Zach Dillon <zdillon90@gmail.com>
2020-04-27 22:27:52 -0400

    commented out firebase for the time being

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { StoreProvider } from 'easy-peasy';
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase';
 import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 require('dotenv').config();
 
-const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: 'battleship-d697e.firebaseapp.com',
-  databaseURL: 'https://battleship-d697e.firebaseio.com',
-  projectId: 'battleship-d697e',
-  storageBucket: '',
-  messagingSenderId: '623731397978',
-  appId: '1:623731397978:web:8c130d3f0ca58c2bf28f63',
-  measurementId: 'G-VS7LB3B941',
-};
-firebase.initializeApp(firebaseConfig);
+// const firebaseConfig = {
+//   apiKey: process.env.APIKEY,
+//   authDomain: 'battleship-d697e.firebaseapp.com',
+//   databaseURL: 'https://battleship-d697e.firebaseio.com',
+//   projectId: 'battleship-d697e',
+//   storageBucket: '',
+//   messagingSenderId: '623731397978',
+//   appId: '1:623731397978:web:8c130d3f0ca58c2bf28f63',
+//   measurementId: 'G-VS7LB3B941',
+// };
+// firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <StoreProvider store={store}>
